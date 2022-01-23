837ca95b018c3796711e84812e93bdf8a2caaf3d
dkatorza <dkatorza@gmail.com>
2022-01-23 10:00:03 +0200

    Adding css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import '../src/assets/styles/styles.scss';
+
 function App() {
   return (
-    <div className='App'>
+    <div className='app-general'>
       <header className='App-header'>
         <h1>Star Wars rebel academy trials</h1>
       </header>
